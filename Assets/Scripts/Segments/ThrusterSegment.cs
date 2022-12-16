20b5ec1f1befea61e9ecf511bb940f238ec30073
Reispfannenfresser <34158734+Reispfannenfresser@users.noreply.github.com>
2022-12-16 21:46:35 +0100

    Made everything fly the other way and generators are thrusters..

--- Assets/Scripts/Segments/ThrusterSegment.cs
+++ Assets/Scripts/Segments/ThrusterSegment.cs
@@ -3,36 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(ConstructionSegment))]
-public class ThrusterSegment : MonoBehaviour {
+public class ThrusterSegment : MonoBehaviour
+{
 	[SerializeField]
 	float max_power = 3f;
 	[SerializeField]
 	float acceleration = 0.1f;
-	float current_power = 0.0001f;
+	float current_power = 0f;
 
 	[SerializeField]
 	private SpriteRenderer fire = null;
 	private ConstructionSegment segment = null;
 
-	private void Awake() {
+	private void Awake()
+	{
 		segment = GetComponent<ConstructionSegment>();
 	}
 
-	private void Start() {
+	private void Start()
+	{
 		segment.fixed_update_wrapper.AddAction("Thruster", OnFixedUpdate);
 	}
 
-	private void OnFixedUpdate(ConstructionSegment segment) {
-		if (current_power < max_power) {
+	private void OnFixedUpdate(ConstructionSegment segment)
+	{
+		if (current_power < max_power)
+		{
 			current_power += Time.deltaTime * acceleration;
-			segment.rb2d.mass = current_power;
+			if (current_power > max_power)
+			{
+				current_power = max_power;
+			}
+			segment.rb2d.gravityScale = -current_power;
+
 			Color c = fire.color;
 			c.a = Mathf.Min(current_power / max_power, 1);
 			fire.color = c;
-		}
-		if (current_power > max_power) {
-			current_power = max_power;
-			segment.rb2d.mass = current_power;
 		}
 	}
 }
